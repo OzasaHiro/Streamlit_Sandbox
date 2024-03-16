7749641dd0fec9a33f99037866cd0926a373ec20
Hiroaki Ozasa <37623652+OzasaHiro@users.noreply.github.com>
2024-03-16 13:58:58 +0900

    Update meeting_minutes_generator.py

--- meeting_minutes_generator.py
+++ meeting_minutes_generator.py
@@ -1,17 +1,19 @@
 import streamlit as st
 from openai import OpenAI
 import anthropic
+import replicate
 import os
 
 def generate_meeting_minutes(text, language, llm, project_name, deadline, budget, customer_name):
     os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
     os.environ["ANTHROPIC_API_KEY"] = st.secrets["ANTHROPIC_API_KEY"]
+    REPLICATE_API_TOKEN=st.secrets["REPLICATE_API_TOKEN"]
 
     if llm == "GPT4":
         LLM = 'gpt-4-0125-preview'
-    elif llm == "GPT3.5":
+    elif llm == "GPTGPT3.5":
         LLM = 'gpt-3.5-turbo-0125'
-    else:
+    elif llm == 'Claude3':
         LLM = 'claude-3-sonnet-20240229' #'claude-3-sonnet-20240229','claude-3-opus-20240229'
 
     summary_prompt = f"""
@@ -64,6 +66,25 @@ def generate_meeting_minutes(text, language, llm, project_name, deadline, budget
                 )
         summary = summary_result.content[0].text
 
+    elif llm == 'Gemma-7B':
+        summary_result = replicate.run(
+                    "google-deepmind/gemma-7b-it:2790a695e5dcae15506138cc4718d1106d0d475e6dca4b1d43f42414647993d5",
+                    input={
+                        "top_k": 50,
+                        "top_p": 0.95,
+                        "prompt": prompt,
+                        "temperature": 0.01,
+                        "max_new_tokens":1000,
+                        "min_new_tokens": 10,
+                        "repetition_penalty": 1
+                    }
+                )
+        output_list = []
+        for item in summary_result:
+            output_list.append(item)
+        
+        summary = "".join(output_list)
+        
     else:
         client = OpenAI()
         summary_result = client.chat.completions.create(model=LLM, messages=[user_request], max_tokens=1000)
